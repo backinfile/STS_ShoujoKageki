dcaddfc2a009771594df8aeab11e341a1c024a68
backinfile <1757518497@qq.com>
2023-12-20 00:49:53 +0800

    add cards

--- src/main/java/ShoujoKageki/cards/BaseCard.java
+++ src/main/java/ShoujoKageki/cards/BaseCard.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import ShoujoKageki.Log;
 import ShoujoKageki.ModInfo;
+import com.megacrit.cardcrawl.localization.CardStrings;
 
 import java.util.List;
 
@@ -16,22 +17,36 @@ public abstract class BaseCard extends AbstractDefaultCard {
     private long previewCardsListLastChangeTime = -1;
     private int previewCardsListIndex = 0;
     private List<AbstractCard> cardsToPreviewList = null;
+    public CardStrings cardStrings;
+    public String DESCRIPTION;
 
     public BaseCard(String id, String img, int cost, AbstractCard.CardType type, AbstractCard.CardRarity rarity, AbstractCard.CardTarget target) {
         super(id, languagePack.getCardStrings(id).NAME, img, cost, languagePack.getCardStrings(id).DESCRIPTION, type, CardColor_Karen, rarity, target);
+        initNameAndDescription(id);
     }
 
     public BaseCard(String id, String img, int cost, AbstractCard.CardType type, AbstractCard.CardColor color, AbstractCard.CardRarity rarity, AbstractCard.CardTarget target) {
         super(id, languagePack.getCardStrings(id).NAME, img, cost, languagePack.getCardStrings(id).DESCRIPTION, type, color, rarity, target);
+        initNameAndDescription(id);
     }
 
     public BaseCard(String id, int cost, AbstractCard.CardType type, AbstractCard.CardRarity rarity, AbstractCard.CardTarget target) {
         super(id, languagePack.getCardStrings(id).NAME, ModInfo.makeCardPath(id.split(":")[1] + ".png"), cost, languagePack.getCardStrings(id).DESCRIPTION, type, CardColor_Karen, rarity, target);
+        initNameAndDescription(id);
     }
 
     public BaseCard(String id, int cost, AbstractCard.CardType type, AbstractCard.CardColor color, AbstractCard.CardRarity rarity, AbstractCard.CardTarget target) {
         super(id, languagePack.getCardStrings(id).NAME, ModInfo.makeCardPath(id.split(":")[1] + ".png"), cost, languagePack.getCardStrings(id).DESCRIPTION, type, color, rarity, target);
+        initNameAndDescription(id);
+    }
 
+    private void initNameAndDescription(String id) {
+        cardStrings = languagePack.getCardStrings(id);
+        DESCRIPTION = cardStrings.DESCRIPTION;
+        this.name = cardStrings.NAME;
+        this.rawDescription = DESCRIPTION;
+        this.initializeTitle();
+        this.initializeDescription();
     }
 
 
--- src/main/java/ShoujoKageki/cards/tool/ToolCard.java
+++ src/main/java/ShoujoKageki/cards/tool/ToolCard.java
@@ -1,11 +1,17 @@
 package ShoujoKageki.cards.tool;
 
+import ShoujoKageki.ModInfo;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import ShoujoKageki.cards.BaseCard;
 import ShoujoKageki.variables.DisposableVariable;
 import ShoujoKageki.variables.patch.DisposableField;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
 
+// 继承此类 自动获得闪耀词条 misc字段占用， 描述更新占用
 public abstract class ToolCard extends BaseCard {
+    public static UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ModInfo.makeID("DisposableKeyword"));
+
     public ToolCard(String id, int cost, CardType type, CardRarity rarity, CardTarget target, int disposableCnt) {
         super(id, cost, type, rarity, target);
         DisposableVariable.setBaseValue(this, disposableCnt);
@@ -28,5 +34,11 @@ public abstract class ToolCard extends BaseCard {
         AbstractCard card = super.makeStatEquivalentCopy();
         DisposableField.DisposableFields.disposable.set(card, this.misc);
         return card;
+    }
+
+    @Override
+    public void initializeDescription() {
+        this.rawDescription = DESCRIPTION + uiStrings.TEXT[0];
+        super.initializeDescription();
     }
 }
